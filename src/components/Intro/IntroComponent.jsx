04f6b0bda77e0a4cb0d880bf76522ea0fd32cd25
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 00:33:34 +0100

    Add IntroComponent

---
+++ src/components/Intro/IntroComponent.jsx
@@ -0,0 +1,10 @@
+import { IntroWrapper, Title, Subtitle } from "./IntroStyled"
+
+export function IntroComponent() {
+  return (
+    <IntroWrapper>
+      <Title>Bienvenido a App Showcase</Title>
+      <Subtitle>Explora nuestras apps y aprende cómo utilizarlas paso a paso</Subtitle>
+    </IntroWrapper>
+  )
+}
---
+++ src/components/Intro/IntroStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const IntroWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  text-align: center;
+  padding: 1rem 0;
+`
+
+export const Title = styled.h1`
+  font-size: 1.6rem;
+`
+
+export const Subtitle = styled.p`
+  font-size: 1rem;
+  opacity: 0.7;
+`
