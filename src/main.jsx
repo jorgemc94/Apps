fe962fd8f006cb8b00a0661d727d15e26293af17
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 00:52:31 +0100

    Fix error main

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-import { Routes, Route, useLocation } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-import { Home } from "./pages/Home/HomePage"
-import { AppDetail } from "./pages/AppDetail/AppDetailPage"
+// main.jsx
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import { ThemeProviderCustom } from "./context/ThemeContext"
+import { App } from "./App"
+import "./App.css"
 
-export function App() {
-  const location = useLocation()
-
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/app/:id" element={<AppDetail />} />
-      </Routes>
-    </AnimatePresence>
-  )
-}
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProviderCustom>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProviderCustom>
+)
