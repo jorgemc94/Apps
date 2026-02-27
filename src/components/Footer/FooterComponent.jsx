4e00309f8dfe6439f770b466004d6b3f92d02ebf
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 00:32:49 +0100

    Add FooterComponent

---
+++ src/components/Footer/FooterComponent.jsx
@@ -0,0 +1,9 @@
+import { FooterWrapper } from "./FooterStyled"
+
+export function FooterComponent() {
+  return (
+    <FooterWrapper>
+      © 2026 App Showcase · Todos los derechos reservados
+    </FooterWrapper>
+  )
+}
---
+++ src/components/Footer/FooterStyled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const FooterWrapper = styled.footer`
+  padding: 1.5rem 1rem;
+  text-align: center;
+  background: var(--card);
+  font-size: 0.85rem;
+  opacity: 0.7;
+`
