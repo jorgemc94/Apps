94b23ca272e57ba677c9154d55da47f235133d07
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 11:47:56 +0100

    add data ListApp

--- src/data/apps.js
+++ src/data/apps.js
@@ -1,9 +1,9 @@
 export const apps = [
   {
-    id: "finance",
-    name: "Finance App",
-    logo: "/AppsGallery/finance.png",
-    description: "Gestiona tus finanzas fácilmente",
+    id: "xpro",
+    name: "XPRO",
+    logo: "/ListApp/XPRO/logo.jpg",
+    description: "Es la apliación que recomiendan, esta apliación solo esta para Android, hay TV´s con android otra opción para tenerla es mediante fire Tv, Andorid box...",
     steps: [
       { image: "finance-step1.png", text: "Descarga la app desde la tienda oficial." },
       { image: "finance-step2.png", text: "Regístrate con tu correo electrónico." },
@@ -11,13 +11,24 @@ export const apps = [
     ]
   },
   {
-    id: "fitness",
-    name: "Fitness App",
-    logo: "/AppsGallery/fitness.png",
-    description: "Entrena y mejora tu salud",
+    id: "ssiptv",
+    name: "SSIPTV",
+    logo: "/ListApp/SSIPTV/logo.jpg",
+    description: "Esta apliación funciona bastante bien, no todas las TV pueden tenerla, si la TV es Samsung se debe añadir mediante USB",
     steps: [
       { image: "fitness-step1.png", text: "Crea tu perfil con tus medidas y objetivos." },
       { image: "fitness-step2.png", text: "Sigue los ejercicios diarios." }
+    ]
+  },
+  {
+    id: "iboplayer",
+    name: "IBO Player",
+    logo: "/ListApp/IboPlayer/logo.jpg",
+    description: "No conozco mucho esta apliación pero alguien la tiene en su TV",
+    steps: [
+      { image: "finance-step1.png", text: "Descarga la app desde la tienda oficial." },
+      { image: "finance-step2.png", text: "Regístrate con tu correo electrónico." },
+      { image: "finance-step3.png" }
     ]
   }
 ]
