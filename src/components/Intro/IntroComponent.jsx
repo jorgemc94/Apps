c6837a9819e965f4852c110b2ae7ac0528472bf2
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 11:09:42 +0100

    fix min error and add new urls

--- src/components/Intro/IntroComponent.jsx
+++ src/components/Intro/IntroComponent.jsx
@@ -5,7 +5,7 @@ export function IntroComponent() {
     <IntroWrapper>
       <Title>ACTUALIZAR LISTAS</Title>
       <Subtitle>
-        Esto es una guia de como actualizar las listas y ponerlas en vuestras aplicaciones. Esto es lo que os hago yo, pero os dejo como podeis hacerlo vosotros sin necesidad de esperar a que os conteste. Si esto no os funciona la solucion es hablar a este telefono de whatsApp <a href="https://wa.me/8613316900374" target="_blank">
+        Esto es una guía de como actualizar las listas y ponerlas en vuestras aplicaciones. Esto es lo que os hago yo, pero os dejo como podéis hacerlo vosotros sin necesidad de esperar a que os conteste. Si esto no os funciona la solución es hablar a este teléfono de whatsApp, no llaméis solo atiende por whatsApp. <a href="https://wa.me/8613316900374" target="_blank">
   Felicia
 </a> 
       </Subtitle>
