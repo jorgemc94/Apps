aeea307568b5bbdb8c592dd791350b4d59559b4f
jorgemc94 <jmaciasc1994@gmail.com>
2026-02-28 09:42:23 +0100

    Modify text intro

--- src/components/Intro/IntroComponent.jsx
+++ src/components/Intro/IntroComponent.jsx
@@ -3,8 +3,12 @@ import { IntroWrapper, Title, Subtitle } from "./IntroStyled"
 export function IntroComponent() {
   return (
     <IntroWrapper>
-      <Title>Bienvenido a App Showcase</Title>
-      <Subtitle>Explora nuestras apps y aprende cómo utilizarlas paso a paso</Subtitle>
+      <Title>ACTUALIZAR LISTAS</Title>
+      <Subtitle>
+        Esto es una guia de como actualizar las listas y ponerlas en vuestras aplicaciones. Esto es lo que os hago yo, pero os dejo como podeis hacerlo vosotros sin necesidad de esperar a que os conteste. Si esto no os funciona la solucion es hablar a este telefono de whatsApp <a href="https://wa.me/8613316900374" target="_blank">
+  Felicia
+</a> 
+      </Subtitle>
     </IntroWrapper>
   )
 }
