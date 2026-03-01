04c842090c97ddf8ea78e9eedfa9ab339fa3372b
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-01 17:41:40 +0100

    Add title AppsGalleryComponent

--- src/components/AppsGallery/AppsGalleryComponent.jsx
+++ src/components/AppsGallery/AppsGalleryComponent.jsx
@@ -1,21 +1,25 @@
 import { useNavigate } from "react-router-dom"
 import { Grid, Card, AppIcon, AppInfo, AppName, AppDescription } from "./AppsGalleryStyled"
+import { Title } from "../../styles/Typography"
 import { apps } from "../../data/apps"
 
 export function AppsGalleryComponent() {
   const navigate = useNavigate()
 
   return (
-    <Grid>
-      {apps.map(app => (
-        <Card key={app.id} onClick={() => navigate(`/app/${app.id}`)}>
-          <AppIcon src={app.logo} alt={app.name} />
-          <AppInfo>
-            <AppName>{app.name}</AppName>
-            <AppDescription>{app.description}</AppDescription>
-          </AppInfo>
-        </Card>
-      ))}
-    </Grid>
+    <>
+      <Title>LISTADO DE APLICACIONES</Title>
+      <Grid>
+        {apps.map(app => (
+          <Card key={app.id} onClick={() => navigate(`/app/${app.id}`)}>
+            <AppIcon src={app.logo} alt={app.name} />
+            <AppInfo>
+              <AppName>{app.name}</AppName>
+              <AppDescription>{app.description}</AppDescription>
+            </AppInfo>
+          </Card>
+        ))}
+      </Grid>
+    </>
   )
 }
