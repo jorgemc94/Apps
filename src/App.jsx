6608a6657b3efa079b7b774030c74594e18aa081
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-01 13:35:51 +0100

    Add ScrollToTop component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { Routes, Route, useLocation } from "react-router-dom"
-import { AnimatePresence } from "framer-motion"
-import { Home } from "./pages/Home/HomePage"
-import { AppDetail } from "./pages/AppDetail/AppDetailPage"
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Home } from "./pages/Home/HomePage";
+import { AppDetail } from "./pages/AppDetail/AppDetailPage";
+import { ScrollToTop } from "./components/ScrollToTop/ScrollToTopComponent";
 
 export function App() {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/app/:id" element={<AppDetail />} />
-      </Routes>
-    </AnimatePresence>
-  )
+    <>
+      <ScrollToTop />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/app/:id" element={<AppDetail />} />
+        </Routes>
+      </AnimatePresence>
+    </>
+  );
 }
