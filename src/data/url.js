7b201f052f08e4474a2dcd1db2776b751d8d7404
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-01 12:39:45 +0100

    Fix min error url

--- src/data/url.js
+++ src/data/url.js
@@ -1,6 +1,6 @@
 export const urls = [
-  { user: "me", url: "https:upcnx.xom/domain_release_upgr.php"},
-  { user: "fe", url: "https:upcnx.xom/domain_release_upgr.php"},
+  { user: "me", url: "https:upcnx.com/domain_release_upgr.php"},
+  { user: "fe", url: "https:upcnx.com/domain_release_upgr.php"},
   { user: "fee", url: "https://greatott.pro/domain_release_konese.php"},
   { user: "fet", url: "https://greatott2.com/domain_release_g21gut.php"},
   { user: "3fe", url: "https://greatott3.club/domain_release_gr31fed.php"},
