8a15ff78d55acaf8a0e1a8e80b9c0c6281dbb37f
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-04 15:31:03 +0100

    add new url and new whatsApp

--- src/components/Intro/IntroComponent.jsx
+++ src/components/Intro/IntroComponent.jsx
@@ -9,8 +9,12 @@ export function IntroComponent() {
       <Subtitle>
         Esto es una guía de cómo actualizar las listas y ponerlas en vuestras aplicaciones. 
         Si esto no os funciona, la solución es hablar a este teléfono de WhatsApp, no llaméis solo atiende por WhatsApp.
-        En el mensaje debéis escribir vuetro nombre de usuario completo, aplicación que estéis usando y decir que no os funciona incluso actualizando la lista. Pero antes de llegar a esto actualizar las listas vosotros. 
+        En el mensaje debéis escribir vuetro nombre de usuario completo, aplicación que estéis usando y decir que no os funciona incluso actualizando la lista. Pero antes de llegar a esto actualizar las listas vosotros.
+        Estos son los dos teléfonos que utilizo para hablar con ellos: 
       </Subtitle>
+      <WhatsApp href="https://wa.me/8618123896049" target="_blank">
+        <FaWhatsapp />
+      </WhatsApp>
       <WhatsApp href="https://wa.me/8613316900374" target="_blank">
         <FaWhatsapp />
       </WhatsApp>
--- src/data/url.js
+++ src/data/url.js
@@ -5,4 +5,5 @@ export const urls = [
   { user: "fet", url: "https://greatott2.com/domain_release_g21gut.php"},
   { user: "3fe", url: "https://greatott3.club/domain_release_gr31fed.php"},
   { user: "ff", url: "https://gtspro.net/domain_release_gtsbnm.php" },
+  { user: "2ff", url: "https://gtspro.net/domain_release_gtsbnm.php"}
 ]
