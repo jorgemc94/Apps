f1e5477b0584e61f5a12f4995e2c6bab5b9154be
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-07 16:03:58 +0100

    update url ff and 2ff

--- src/data/url.js
+++ src/data/url.js
@@ -4,6 +4,6 @@ export const urls = [
   { user: "fee", url: "https://greatott.pro/domain_release_konese.php"},
   { user: "fet", url: "https://greatott2.com/domain_release_g21gut.php"},
   { user: "3fe", url: "https://greatott3.club/domain_release_gr31fed.php"},
-  { user: "ff", url: "https://gtspro.net/domain_release_gtsbnm.php" },
-  { user: "2ff", url: "https://gtspro.net/domain_release_gtsbnm.php"}
+  { user: "ff", url: "https://gtspro.net/domain_release_gtmar.php" },
+  { user: "2ff", url: "https://gtspro.net/domain_release_gtmar.php"}
 ]
