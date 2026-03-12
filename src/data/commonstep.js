78b479f4e15a5ae5fb1bb87e13817ce5070af3bc
jorgemc94 <jmaciasc1994@gmail.com>
2026-03-12 19:03:06 +0100

    add video commonstep

--- src/data/commonstep.js
+++ src/data/commonstep.js
@@ -1,6 +1,6 @@
 export const commonstep = [
     {
         text: "Copiar el mensaje de WhatsApp en las notas del móvil, así podéis copiar mejor los datos necesarios. Abrir el enlace de la web en Chrome",
-        video: "/CommonStep/"
+        video: "/CommonStep/CommonStep.mp4"
     }
 ]
